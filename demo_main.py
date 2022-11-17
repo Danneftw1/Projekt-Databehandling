6b349392b6d536b7f8111abf809c3c981677f7fd
Canesp <philip.decanesie@gmail.com>
2022-11-17 15:44:13 +0100

    add a layout py file with start of layout

---
+++ demo_main.py
@@ -0,0 +1,18 @@
+import dash
+import dash_bootstrap_components as dbc
+from dash import html, dcc
+from dash.dependencies import Output, Input
+from layout import Layout
+
+
+# creates the dash app
+app = dash.Dash(
+    __name__,
+    external_stylesheets=[dbc.themes.MATERIA],
+    meta_tags=[dict(name="viewport", content="width=device-width, initial-scale=1.0")],
+)
+
+app.layout = Layout().layout()
+
+if __name__ == "__main__":
+    app.run_server(debug=True)
---
+++ layout.py
@@ -0,0 +1,24 @@
+from dash import html, dcc
+import dash_bootstrap_components as dbc
+
+class Layout:
+
+    def layout(self):
+
+        return dbc.Container(
+            [html.Header([html.H1("Sweden OS")]),
+             
+             dbc.Row(children=[
+
+                dbc.Col(
+                    dbc.Container([html.P("Con1")], id="con1", fluid= True), width= 9
+                ), 
+
+                dbc.Col(
+                    dbc.Container([html.P("Con2")], id="con2", fluid= True), 
+                ),
+                
+                
+             ],className="g-0", justify="start",)
+            
+            ], fluid= True)
